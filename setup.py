00a5cbe26468b08ecd64423bdd74ea12d823572b
Jan Lipovský <janlipovsky@gmail.com>
2017-03-15 23:43:19 +0100

    Adding python 3.3

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
                  "Intended Audience :: Developers",
                  "License :: OSI Approved :: MIT License",
                  "Programming Language :: Python",
+                 'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
