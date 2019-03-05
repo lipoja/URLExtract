588f9450d90f742079f83391cc114365484bd930
Jan Lipovský <janlipovsky@gmail.com>
2019-03-05 23:22:12 +0100

    Setting version to 0.10-pre-alpha

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 script_dirname = join(dirname(__file__))
 
 # version of URLExtract (do not forget to change it in urlextract_core.py as well)
-__version__ = '0.9'
+__version__ = '0.10-pre-alpha'
 
 
 def read(readme):
