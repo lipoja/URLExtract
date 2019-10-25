5c1ec1a8592a4018420d37700acdf2ce0dfc88c3
Jan Lipovsky <janlipovsky@gmail.com>
2019-10-25 21:30:35 +0200

    Added support of parsing IPv4 addresses (issue #10)

---
+++ tests/unit/test_ipv4.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+.. Licence MIT
+.. codeauthor:: Jan Lipovský <janlipovsky@gmail.com>, janlipovsky.cz
+"""
+import pytest
+
+
+@pytest.mark.parametrize("text, expected", [
+    ("http://127.0.0.1/",
+     ['http://127.0.0.1/']),
+
+    ("http://192.168.1.1/",
+     ['http://192.168.1.1/']),
+
+    ("http://192.168.1.255/test.html",
+     ['http://192.168.1.255/test.html']),
+
+    # square brackets defines IPv6 and later
+    # therefore only IP is returned because only valid parts is within []
+    ("http://[192.168.1.1]/", ['192.168.1.1']),
+
+    ("http://192.1.1/", [])
+])
+def test_find_urls(urlextract, text, expected):
+    """
+    Testing find_urls returning all URLs
+
+    :param fixture urlextract: fixture holding URLExtract object
+    :param str text: text in which we should find links
+    :param list(str) expected: list of URLs that has to be found in text
+    """
+    assert urlextract.find_urls(text) == expected
