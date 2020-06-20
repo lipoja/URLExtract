6d1ad3fd6e55cc17ecfde07ee97177be9db61eba
Jan Lipovský <janlipovsky@gmail.com>
2020-06-20 23:11:44 +0200

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ script_dirname = join(dirname(__file__))
 
 # version of URLExtract
 # (do not forget to change it in urlextract_core.py as well)
-__version__ = '0.14.0'
+__version__ = '1.0.0'
 
 
 def read(readme):
