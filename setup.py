fa6c9d2334431193c029b0e316ba5b43f9958d71
Jan Lipovský <janlipovsky@gmail.com>
2020-12-08 12:36:26 +0100

    Set as Production/Stable in setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     long_description=read('README.rst'),
     long_description_content_type='text/x-rst',
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
