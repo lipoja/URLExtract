3b285074170df9279f8f490d3625ec77392ec511
Vladimir Baev <extless@gmail.com>
2022-04-29 13:02:39 +0700

    Add `host limit list` tests

---
+++ tests/unit/test_host_limit_list.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This file contains pytests for find_urls() method of URLExtract with
+list of URLs that can be processed
+
+.. Licence MIT
+.. codeauthor:: khoben <extless@gmail.com>
+"""
+import pytest
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("example.com", ["example.com"]),
+        ("ample.com", []),
+        ("another-url.com", ["another-url.com"]),
+        ("one-another-url.com", []),
+        ("http://example.com", ["http://example.com"]),
+        ("http://example.com:1234", ["http://example.com:1234"]),
+        ("admin@example.com", []),
+        ("ftp://admin:pass@example.com", ["ftp://admin:pass@example.com"]),
+        (
+            "http://subdom.example.com:1234/path/file.html?query=something#hashtag",
+            [],
+        ),
+        ("www.example.com", []),
+        ("example.net", []),
+    ],
+)
+def test_host_limit_list(urlextract, text, expected):
+    """
+    Testing find_urls with list of URLs that can be processed
+
+    :param fixture urlextract: fixture holding URLExtract object
+    :param str text: text in which we should find links
+    :param list(str) expected: list of URLs that has to be found in text
+    """
+    urlextract.host_limit_list = {"example.com", "another-url.com"}
+    assert expected == urlextract.find_urls(text)
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("example.com", ["example.com"]),
+        ("ample.com", []),
+        ("another-url.com", []),
+        ("one-another-url.com", []),
+        ("http://example.com", ["http://example.com"]),
+        ("http://example.com:1234", ["http://example.com:1234"]),
+        ("admin@example.com", []),
+        ("ftp://admin:pass@example.com", ["ftp://admin:pass@example.com"]),
+        (
+            "http://subdom.example.com:1234/path/file.html?query=something#hashtag",
+            [],
+        ),
+        ("www.example.com", []),
+        ("example.net", []),
+    ],
+)
+def test_host_limit_list_with_ignore(urlextract, text, expected):
+    """
+    Testing find_urls with list of URLs list that can be processed and URLs ignore list
+
+    :param fixture urlextract: fixture holding URLExtract object
+    :param str text: text in which we should find links
+    :param list(str) expected: list of URLs that has to be found in text
+    """
+    urlextract.host_limit_list = {"example.com", "another-url.com"}
+    urlextract.ignore_list = {"another-url.com"}
+    assert expected == urlextract.find_urls(text)
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("example.com", ["example.com"]),
+        ("ample.com", []),
+        ("another-url.com", ["another-url.com"]),
+        ("one-another-url.com", []),
+        ("http://example.com", ["http://example.com"]),
+        ("http://example.com:1234", ["http://example.com:1234"]),
+        ("admin@example.com", ["admin@example.com"]),
+        ("ftp://admin:pass@example.com", ["ftp://admin:pass@example.com"]),
+        (
+            "http://subdom.example.com:1234/path/file.html?query=something#hashtag",
+            [],
+        ),
+        ("www.example.com", []),
+        ("example.net", []),
+    ],
+)
+def test_host_limit_list_with_emails(urlextract, text, expected):
+    """
+    Testing find_urls with list of URLs that can be processed with email addresses
+
+    :param fixture urlextract: fixture holding URLExtract object
+    :param str text: text in which we should find links
+    :param list(str) expected: list of URLs that has to be found in text
+    """
+    urlextract.extract_email = True
+    urlextract.host_limit_list = {"example.com", "another-url.com"}
+    assert expected == urlextract.find_urls(text)
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("Local development url http://localhost:8000/ and www.example.com", ["http://localhost:8000/"]),
+        ("Some text with  localhost in it", []),
+    ],
+)
+def test_host_limit_localhost_enabled(urlextract, text, expected):
+    """
+    Testing find_urls with list of URLs that can be processed with localhost
+
+    :param fixture urlextract: fixture holding URLExtract object
+    :param str text: text in which we should find links
+    :param list(str) expected: list of URLs that has to be found in text
+    """
+    urlextract.host_limit_list = {"localhost"}
+    assert expected == urlextract.find_urls(text)
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("Local development url http://localhost:8000/", []),
+        ("Some text with  localhost in it", []),
+    ],
+)
+def test_host_limit_localhost_disabled(urlextract, text, expected):
+    """
+    Testing find_urls with list of URLs that can be processed without localhost
+
+    :param fixture urlextract: fixture holding URLExtract object
+    :param str text: text in which we should find links
+    :param list(str) expected: list of URLs that has to be found in text
+    """
+    urlextract.extract_localhost = False
+    urlextract.host_limit_list = {"localhost"}
+    assert expected == urlextract.find_urls(text)
+
